2f34b2b64b807793a94a96ee519d3cbf4ce454f8
abdellah-elazzaoui <elazzaoui859@gmail.com>
2026-07-06 00:08:54 +0100

    change api

--- api.py
+++ api.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from runner import runner
-app = FastAPI()
+from pydantic import BaseModel,Field
+
+app = FastAPI(title="EMC Helpline Assistant")
+
+
 @app.get("/chat")
 def chat_model(question:str):
-    response = runner(question=question)
+    if not question:
+        return HTTPException(
+            status_code=400,
+            detail="La question ne peut pas être vide."
+        )
+    try:
+        response = runner(question=question)
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail= f"Une erreur est survenue lors du traitement de votre question: {e}"
+        )    
     return {"message":response}

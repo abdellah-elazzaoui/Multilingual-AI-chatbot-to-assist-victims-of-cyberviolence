4face0c1d2e7e9af3af6dd9e70c286304439e2a9
abdellah-elazzaoui <elazzaoui859@gmail.com>
2026-07-05 01:19:29 +0100

    retriever

---
+++ runner.py
@@ -0,0 +1,50 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from retrever import retriever
+
+model_name = "qwen2.5:3b"
+llm = ChatOllama(
+    model=model_name,
+    temperature = 0
+)
+
+template = """
+Vous êtes EMC Helpline, un assistant IA spécialisé dans l'assistance aux victimes de cyberviolences.
+
+Votre mission est d'aider les utilisateurs avec bienveillance, respect et professionnalisme.
+
+Règles importantes :
+- Soyez toujours poli, empathique et rassurant.
+- Répondez de manière claire, concise et facile à comprendre.
+- Utilisez UNIQUEMENT les informations présentes dans le contexte fourni.
+- Ne créez jamais d'informations, de lois, de procédures ou de conseils qui ne figurent pas dans le contexte.
+- Si le contexte ne contient pas la réponse, dites simplement :
+  "Je suis désolé, je ne dispose pas de cette information dans ma base de connaissances. Je vous invite à consulter les ressources officielles ou à contacter un organisme compétent."
+- Ne mentionnez jamais que vous êtes un modèle de langage ou une IA.
+- Si la question est ambiguë, demandez poliment des précisions avant de répondre.
+- Répondez toujours dans la même langue que la question de l'utilisateur (français ou arabe).
+- Si le contexte contient plusieurs informations pertinentes, combinez-les pour fournir une réponse complète et cohérente.
+- Si possible, structurez votre réponse avec des listes à puces pour améliorer la lisibilité.
+
+Contexte :
+{context}
+
+Question :
+{question}
+
+Réponse :
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = prompt | llm
+
+while True:
+    question = input(" Enter your question (in Arabic or French): ")
+    if str(question).lower() in ['q','quit','0','exit']:
+        break
+    context = retriever.invoke(question)
+    result = chain.invoke({"context":context , "question":question})
+    print(f"Assistant: ")
+    print(result.content)
+    print("----------------------")
